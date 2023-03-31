fc4f331ae1652c9d4eeb30e83054728dca8a920d
yeonghoon.lee <djaakclsrn12@naver.com>
2023-03-31 18:40:01 +0900

    NEW: Bean수동 등록을 위한 Config파일 생성

--- src/main/java/hello/advanced/AdvancedApplication.java
+++ src/main/java/hello/advanced/AdvancedApplication.java
@@ -1,9 +1,12 @@
 package hello.advanced;
 
+import hello.advanced.proxy.config.AppV1Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import(AppV1Config.class)
+@SpringBootApplication(scanBasePackages = "hello.advanced.proxy.app") // 지정해주지 않으면 Application이 존재하는 곳에서 패키징
 public class AdvancedApplication {
 
 	public static void main(String[] args) {
