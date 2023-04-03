d23bbb0a82d6a2ac1efc6f6c4e4668d040e4fe1e
yeonghoon.lee <djaakclsrn12@naver.com>
2023-04-03 16:51:44 +0900

    NEW: 구체클래스 기반 프록시 테스트

---
+++ src/test/java/hello/advanced/proxy/concreteproxy/ConcreteProxyTest.java
@@ -0,0 +1,19 @@
+package hello.advanced.proxy.concreteproxy;
+
+import hello.advanced.proxy.concreteproxy.code.ConcreteClient;
+import hello.advanced.proxy.concreteproxy.code.ConcreteLogic;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 구체클래스 기반 프록시 테스트
+ * (인터페이스 없이, 클라이언트 코드변경 없이 프록시패턴 사용)
+ */
+public class ConcreteProxyTest {
+
+    @Test
+    void noProxy(){
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        ConcreteClient concreteClient = new ConcreteClient(concreteLogic);
+        concreteClient.execute();
+    }
+}
