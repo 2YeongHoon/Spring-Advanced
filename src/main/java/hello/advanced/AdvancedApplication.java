5f4299974047eeac8f84f3638937ae144d74d630
2Yeonghoon <djaakclsrn12@naver.com>
2023-04-23 22:44:41 +0900

    NEW: JDK 동적프록시 적용

--- src/main/java/hello/advanced/AdvancedApplication.java
+++ src/main/java/hello/advanced/AdvancedApplication.java
@@ -1,13 +1,18 @@
 package hello.advanced;
 
-import hello.advanced.proxy.config.AppV1Config;
-import hello.advanced.proxy.config.AppV2Config;
+import hello.advanced.proxy.config.v1_proxy.ConcreteProxyConfig;
+import hello.advanced.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 //@Import(AppV1Config.class)
-@Import({AppV1Config.class, AppV2Config.class})
+//@Import({AppV1Config.class, AppV2Config.class})
+//@Import(ConcreteProxyConfig.class)
+@Import(DynamicProxyBasicConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.advanced.proxy.app") // 지정해주지 않으면 Application이 존재하는 곳에서 패키징
 public class AdvancedApplication {
 
@@ -15,4 +20,8 @@ public class AdvancedApplication {
 		SpringApplication.run(AdvancedApplication.class, args);
 	}
 
+	@Bean
+	public LogTrace logTrace(){
+		return new ThreadLocalLogTrace();
+	}
 }
