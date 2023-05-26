7cf8e8402b9eaf46a9fcdbcdf19ca410cbff78b6
2Yeonghoon <djaakclsrn12@naver.com>
2023-05-26 15:07:09 +0900

    NEW: 빈후처리기 적용

--- src/main/java/hello/advanced/AdvancedApplication.java
+++ src/main/java/hello/advanced/AdvancedApplication.java
@@ -2,6 +2,7 @@ package hello.advanced;
 
 import hello.advanced.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.advanced.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+import hello.advanced.proxy.config.v5_autoproxy.AutoProxyConfig;
 import hello.advanced.trace.logtrace.LogTrace;
 import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -16,7 +17,8 @@ import org.springframework.context.annotation.Import;
 //@Import(DynamicProxyFilterConfig.class)
 //@Import(ProxyFactoryConfigV1.class)
 //@Import(ProxyFactoryConfigV2.class)
-@Import(BeanPostProcessorConfig.class)
+//@Import(BeanPostProcessorConfig.class)
+@Import(AutoProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.advanced.proxy.app") // 지정해주지 않으면 Application이 존재하는 곳에서 패키징
 public class AdvancedApplication {
 
