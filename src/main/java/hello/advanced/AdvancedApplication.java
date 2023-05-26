acc133f2a0c889fb715395d5855044e6d71887ea
2Yeonghoon <djaakclsrn12@naver.com>
2023-05-26 17:00:45 +0900

    NEW: @Aspect을 활용한 LogTrace적용

--- src/main/java/hello/advanced/AdvancedApplication.java
+++ src/main/java/hello/advanced/AdvancedApplication.java
@@ -3,6 +3,7 @@ package hello.advanced;
 import hello.advanced.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.advanced.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
 import hello.advanced.proxy.config.v5_autoproxy.AutoProxyConfig;
+import hello.advanced.proxy.config.v6_aop.AopConfig;
 import hello.advanced.trace.logtrace.LogTrace;
 import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -18,7 +19,8 @@ import org.springframework.context.annotation.Import;
 //@Import(ProxyFactoryConfigV1.class)
 //@Import(ProxyFactoryConfigV2.class)
 //@Import(BeanPostProcessorConfig.class)
-@Import(AutoProxyConfig.class)
+//@Import(AutoProxyConfig.class)
+@Import(AopConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.advanced.proxy.app") // 지정해주지 않으면 Application이 존재하는 곳에서 패키징
 public class AdvancedApplication {
 
